b53685ebf83403a9025edf31bbb22f3a7e856cdc
Pavithra Vemula <pavithravemula04@gmail.com>
2022-12-06 19:07:08 +0530

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HMS_Testing.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS_Testing.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+       
+
+
+    }
+}
